9c99c02f49e8ec9c33083ba92e2d83963cb66798
osef <basilelegal@gmail.com>
2020-11-03 20:53:43 +0100

    lint(home): eslint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,204 +7,203 @@
       ref="form"
       lazy-validation
     >
-     <v-card-text>
+      <v-card-text>
         <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <v-select
-            v-model="profileId"
-            :items="profiles"
-            :rules="[v => !!v || 'Profil requis']"
-            item-value="id"
-            item-text="profile_name"
-            label="Profil"
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-select
+                v-model="profileId"
+                :items="profiles"
+                :rules="[v => !!v || 'Profil requis']"
+                item-value="id"
+                item-text="profile_name"
+                label="Profil"
+              />
+            </v-col>
+            <router-link
+              :to="{name: 'NewProfile'}"
+              class="newProfile"
+            >
+              Ajouter un nouveau profil
+            </router-link>
+          </v-row>
+
+          <v-subheader>
+            Date et heure de création (pour antidater le document)
+          </v-subheader>
+          <v-divider />
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-menu
+                v-model="times.creationDateMenu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="times.creationDateFormatted"
+                    label="Date"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  />
+                </template>
+                <v-date-picker
+                  v-model="times.creationDate"
+                  locale="fr-fr"
+                  @input="times.creationDateMenu = false"
+                />
+              </v-menu>
+            </v-col>
+
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-menu
+                ref="menu"
+                v-model="times.creationTimeMenu"
+                :close-on-content-click="false"
+                :return-value.sync="times.creationTime"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="times.creationTime"
+                    label="Heure"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  />
+                </template>
+                <v-time-picker
+                  v-if="times.creationTimeMenu"
+                  v-model="time"
+                  format="24hr"
+                  full-width
+                  @click:minute="$refs.menu.save(times.creationTime)"
+                />
+              </v-menu>
+            </v-col>
+          </v-row>
+
+          <v-subheader>
+            Date et heure de sortie
+          </v-subheader>
+          <v-divider />
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-menu
+                v-model="times.dateMenu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="times.dateFormatted"
+                    label="Date"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  />
+                </template>
+                <v-date-picker
+                  v-model="times.date"
+                  locale="fr-fr"
+                  @input="times.dateMenu = false"
+                />
+              </v-menu>
+            </v-col>
+
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-menu
+                ref="menu"
+                v-model="times.timeMenu"
+                :close-on-content-click="false"
+                :return-value.sync="times.time"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="times.time"
+                    label="Heure"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  />
+                </template>
+                <v-time-picker
+                  v-if="times.timeMenu"
+                  v-model="time"
+                  format="24hr"
+                  full-width
+                  @click:minute="$refs.menu.save(times.time)"
+                />
+              </v-menu>
+            </v-col>
+          </v-row>
+          <v-subheader>
+            Raisons
+          </v-subheader>
+          <v-divider />
+          <v-radio-group
+            v-model="reason"
+            row
+            hint="L'option carrottes est à vos risques et périls, ce sera visible par les forces de l'ordre"
+            :persistent-hint="true"
+          >
+            <v-radio
+              v-for="reason in reasons"
+              :key="reason.code"
+              :label="reason.label"
+              :value="reason"
+            />
+          </v-radio-group>
+
+          <v-img
+            v-if="reason.code === 'achat_de_carrottes'"
+            src="../assets/carrottes.jpg"
+            width="400px"
           />
-        </v-col>
-        <router-link
-          :to="{name: 'NewProfile'}"
-          class="newProfile"
-        >
-          Ajouter un nouveau profil
-        </router-link>
-      </v-row>
-
-      <v-subheader>
-        Date et heure de création (pour antidater le document)
-      </v-subheader>
-      <v-divider />
-      <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <v-menu
-            v-model="times.creationDateMenu"
-            :close-on-content-click="false"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="times.creationDateFormatted"
-                label="Date"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              />
-            </template>
-            <v-date-picker
-              v-model="times.creationDate"
-              locale="fr-fr"
-              @input="times.creationDateMenu = false"
-            />
-          </v-menu>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <v-menu
-            ref="menu"
-            v-model="times.creationTimeMenu"
-            :close-on-content-click="false"
-            :return-value.sync="times.creationTime"
-            transition="scale-transition"
-            offset-y
-            max-width="290px"
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="times.creationTime"
-                label="Heure"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              />
-            </template>
-            <v-time-picker
-              v-if="times.creationTimeMenu"
-              v-model="time"
-              format="24hr"
-              full-width
-              @click:minute="$refs.menu.save(times.creationTime)"
-            />
-          </v-menu>
-        </v-col>
-      </v-row>
-
-      <v-subheader>
-        Date et heure de sortie
-      </v-subheader>
-      <v-divider />
-      <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <v-menu
-            v-model="times.dateMenu"
-            :close-on-content-click="false"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="times.dateFormatted"
-                label="Date"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              />
-            </template>
-            <v-date-picker
-              v-model="times.date"
-              locale="fr-fr"
-              @input="times.dateMenu = false"
-            />
-          </v-menu>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <v-menu
-            ref="menu"
-            v-model="times.timeMenu"
-            :close-on-content-click="false"
-            :return-value.sync="times.time"
-            transition="scale-transition"
-            offset-y
-            max-width="290px"
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="times.time"
-                label="Heure"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              />
-            </template>
-            <v-time-picker
-              v-if="times.timeMenu"
-              v-model="time"
-              format="24hr"
-              full-width
-              @click:minute="$refs.menu.save(times.time)"
-            />
-          </v-menu>
-        </v-col>
-      </v-row>
-      <v-subheader>
-        Raisons
-      </v-subheader>
-      <v-divider />
-      <v-radio-group
-        v-model="reason"
-        row
-        hint="L'option carrottes est à vos risques et périls, ce sera visible par les forces de l'ordre"
-        :persistent-hint="true"
-      >
-        <v-radio
-          v-for="reason in reasons"
-          :key="reason.code"
-          :label="reason.label"
-          :value="reason"
-        />
-      </v-radio-group>
-
-      <v-img
-        v-if="reason.code === 'achat_de_carrottes'"
-        src="../assets/carrottes.jpg"
-        width="400px"
-      />
-
-      </v-container>
+        </v-container>
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-      <v-btn
-        class="mr-4"
-        text
-        color="primary"
-        @click="generate"
-      >
-        Générer
-      </v-btn>
+        <v-btn
+          class="mr-4"
+          text
+          color="primary"
+          @click="generate"
+        >
+          Générer
+        </v-btn>
       </v-card-actions>
     </v-form>
-    </v-card>
+  </v-card>
 </template>
 
 <script>
